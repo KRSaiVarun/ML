f9061f6fb7b2897c54a142c9ecf4950101c0ab3b
K.R SAI VARUN <krsaivarun@gmail.com>
2025-07-16 13:34:21 +0530

    Update DecisionTreeClassifier.py

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -2,17 +2,15 @@ from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 import matplotlib.pyplot as plt
 
-# Get iris flower data
 iris = load_iris()
 
-# Make the decision tree model
 tree = DecisionTreeClassifier()
 tree.fit(iris.data, iris.target)
 
-# Draw the tree
 plt.figure(figsize=(10, 6))
 plot_tree(tree, 
           feature_names=iris.feature_names,
           class_names=iris.target_names,
           filled=True)
+plt.title("Decision Tree Visualization")
 plt.show()
